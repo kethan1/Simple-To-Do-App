8e48e5f039102c17a74a5f594a3f8f39dd928603
Kethan Vegunta <kethan@vegunta.com>
2021-01-21 16:23:36 -0800

    speed up app

--- src/render.js
+++ src/render.js
@@ -1,10 +1,19 @@
-const Store = require("electron-store");
+// const Store = require("electron-store");
 
-const store = new Store();
+var Store = null;
+var store = null;
+
+function get_store() {
+    if (Store === null) {
+        Store = require("electron-store");
+        store = new Store();
+    }
+    return store;
+}
 
 function AddList(listName) {
-    if (store.get(`Simple-To-Do-App.${listName}`) === undefined) {
-        store.set(`Simple-To-Do-App.${listName}`, []);
+    if (get_store().get(`Simple-To-Do-App.${listName}`) === undefined) {
+        get_store().set(`Simple-To-Do-App.${listName}`, []);
         return "success"
     } else {
         return "name_taken"
@@ -12,23 +21,23 @@ function AddList(listName) {
 }
 
 function GetLists() {
-    return store.get("Simple-To-Do-App");
+    return get_store().get("Simple-To-Do-App");
 }
 
 function AddItemtoList(listName, item) {
-    var tmp_array = store.get(`Simple-To-Do-App.${listName}`);
+    var tmp_array = get_store().get(`Simple-To-Do-App.${listName}`);
     tmp_array.push(item);
-    store.set(`Simple-To-Do-App.${listName}`, tmp_array);
+    get_store().set(`Simple-To-Do-App.${listName}`, tmp_array);
 }
 
 function RemoveItemfromList(listName, item) {
-    var tmp_array = store.get(`Simple-To-Do-App.${listName}`);
+    var tmp_array = get_store().get(`Simple-To-Do-App.${listName}`);
     var tmp_array2 = tmp_array.filter(function(value) { 
         return value !== item;
     });
-    store.set(`Simple-To-Do-App.${listName}`, tmp_array2);
+    get_store().set(`Simple-To-Do-App.${listName}`, tmp_array2);
 }
 
 function DeleteList(listName) {
-    store.delete(`Simple-To-Do-App.${listName}`)
+    get_store().delete(`Simple-To-Do-App.${listName}`)
 }
