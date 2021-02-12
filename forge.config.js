dfbf2e174a54e70dbb4824f636939635118325d5
Kethan Vegunta <kethan@vegunta.com>
2021-02-11 19:07:26 -0800

    fix github actions

--- forge.config.js
+++ forge.config.js
@@ -11,9 +11,7 @@ module.exports = {
         {
             "name": "@electron-forge/maker-zip",
             "platforms": [
-                "darwin",
-                "win32",
-                "linux"
+                "darwin"
             ]
         },
         {
